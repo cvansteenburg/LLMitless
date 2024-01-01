5fe8aa156759fd85fc2dfca02d1f400760c9bd6d
Chase <cvansteenburg@gmail.com>
2024-01-01 17:28:46 -0500

    Add Pydantic classes for POST body validation

--- serve.py
+++ serve.py
@@ -1,12 +1,12 @@
 import os
-from typing import Annotated, Any, Coroutine
+from typing import Any, Coroutine
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, Header, HTTPException, status
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from langchain.callbacks import get_openai_callback
 from langchain_core.documents import Document
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 from src.chains.map_reduce import map_reduce
 from src.models.dataset_model import DatasetFileFormatNames
@@ -30,7 +30,7 @@ env_file = (
 load_dotenv(env_file)
 
 
-app = FastAPI
+app = FastAPI()
 
 CONFIG_FILE = "pyproject.toml"
 logger = init_logging(CONFIG_FILE)
@@ -48,128 +48,117 @@ async def root():
 # Summarize
 # Specify format
 
+# PLACEHOLDER. May later want to include... system_prompt: Annotated[str | None, Field(title="system prompt", description="Provides a role, context, and instructions for the LLM. This is a string with brackets around template variables. Must include {core_prompt}")] = None,
+
+
+class FileFilter(BaseModel):
+    collection_digits: str = Field(
+        ...,
+        title="Collection digits",
+        description='Usually a 3 digit number expressed as a string eg. "010"',
+    )
+    title_digits: list[str] = Field(
+        ...,
+        title="Title digits",
+        description=(
+            'A list of usually 3 digit numbers expressed as a strings eg. ["001",'
+            ' "002", "009"]'
+        ),
+    )
+
+
+class Preprocessor(BaseModel):
+    max_tokens_per_doc: int = Field(
+        default=3000,
+        title="Max tokens per doc",
+        description=(
+            "The maximum number of tokens to include in each doc that the LLM will"
+            " summarize. Change this to according the context window of the LLM and the"
+            " length of the prompt."
+        ),
+    )
+    metadata_to_include: list[str] | None = Field(
+        default=None,
+        title="Metadata passed to LLM",
+        description=(
+            "In a map-reduce summarization strategy, docs are combined and presented"
+            " together to the llm. The metadata keys are included in the combined"
+            " documents to give the LLM more context."
+        ),
+    )
+
+
+class SummarizeMapReduce(BaseModel):
+    core_prompt: str | None = Field(
+        default=None,
+        title="core prompt",
+        description=(
+            "Provides a role, context, and instructions for the LLM. LLM will summarize"
+            " the content using this prompt. This is a string with brackets around"
+            " template variables. At a minimum, prompt should include page_content in"
+            " brackets like this: {page_content} where the contents of each summarized"
+            " document will be inserted."
+        ),
+    )
+    collapse_prompt: str | None = Field(
+        default=None,
+        title="collapse prompt",
+        description=(
+            "In a map-reduce summarization strategy, the LLM will summarize the"
+            " summaries to reduce the total amount of text. LLM will use this prompt to"
+            " summarize the summaries. Use template variables {core_prompt} to include"
+            " the core prompt, and {context} wherever the summaries to summarize will"
+            " be inserted."
+        ),
+    )
+    combine_prompt: str | None = Field(
+        default=None,
+        title="combine prompt",
+        description=(
+            "In a map-reduce summarization strategy, the last step is for the LLM to"
+            " combine the summaries. LLM will use this prompt to combine the summaries."
+            " Use template variables {core_prompt} to include the core prompt, and"
+            " {context} wherever the list of summaries to combine will be inserted."
+        ),
+    )
+    max_concurrency: int = Field(
+        default=3,
+        title="Max concurrency",
+        description=(
+            "Maximum number of parallel calls to the LLM the summarizer is allowed to"
+            " make."
+        ),
+    )
+    iteration_limit: int = Field(
+        default=3,
+        title="Iteration limit",
+        description=(
+            "In a map-reduce summarization strategy, this is the maximum number of"
+            ' times the LLM will "summarize the summaries".'
+        ),
+    )
+    collapse_token_max: int = Field(
+        default=6000,
+        title="Collapse token max",
+        description=(
+            "In a map-reduce summarization strategy, this is the maximum number of"
+            " tokens to include in the combined summaries that the LLM will summarize."
+        ),
+    )
+
 
 @app.post("/summarize_from_disk")
 async def summarize_from_disk(
-    collection_digits: Annotated[
-        str,
-        Field(
-            title="Collection digits",
-            description='Usually a 3 digit number expressed as a string eg. "010"',
-        ),
-    ],
-    title_digits: Annotated[
-        list[str],
-        Field(
-            title="Title digits",
-            description=(
-                'A list of usually 3 digit numbers expressed as a strings eg. ["001,'
-                ' "002", "009"]'
-            ),
-        ),
-    ],
-    core_prompt: Annotated[
-        str,
-        Field(
-            title="core prompt",
-            description=(
-                "Provides a role, context, and instructions for the LLM. LLM will"
-                " summarize the content using this prompt. This is a string with"
-                " brackets around template variables. At a minimum, prompt should"
-                " include page_content in brackets like this: {page_content} where the"
-                " contents of each summarized document will be inserted."
-            ),
-        ),
-    ] = None,
-    api_key: Annotated[
-        str | None,
-        Header(
-            title="API key",
-            description="Pass an API key for the summarization LLM. OpenAI is default",
-        ),
-    ] = None,
-    # PLACEHOLDER. May later want to include... system_prompt: Annotated[str | None, Field(title="system prompt", description="Provides a role, context, and instructions for the LLM. This is a string with brackets around template variables. Must include {core_prompt}")] = None,
-    collapse_prompt: Annotated[
-        str | None,
-        Field(
-            title="collapse prompt",
-            description=(
-                "In a map-reduce summarization strategy, the LLM will summarize the"
-                " summaries to reduce the total amount of text. LLM will use this"
-                " prompt to summarize the summaries. Use template variables"
-                " {core_prompt} to include the core prompt, and {context} wherever the"
-                " summaries to summarize will be inserted."
-            ),
-        ),
-    ] = None,
-    combine_prompt: Annotated[
-        str | None,
-        Field(
-            title="combine prompt",
-            description=(
-                "In a map-reduce summarization strategy, the last step is for the LLM"
-                " to combine the summaries. LLM will use this prompt to combine the"
-                " summaries. Use template variables {core_prompt} to include the core"
-                " prompt, and {context} wherever the list of summaries to combine will"
-                " be inserted."
-            ),
-        ),
-    ] = None,
-    *,
-    max_concurrency: Annotated[
-        int,
-        Field(
-            title="Max concurrency",
-            description=(
-                "Maximum number of parallel calls to the LLM the summarizer is allowed"
-                " to make."
-            ),
-        ),
-    ] = 3,
-    max_tokens_per_doc: Annotated[
-        int,
-        Field(
-            title="Max tokens per doc",
-            description=(
-                "The maximum number of tokens to include in each doc that the LLM will"
-                " summarize. Change this to according the context window of the LLM and"
-                " the length of the prompt."
-            ),
-        ),
-    ] = 3000,
-    metadata_to_include: Annotated[
-        list[str],
-        Field(
-            title="Metadata passed to LLM",
-            description=(
-                "In a map-reduce summarization strategy, docs are combined and"
-                " presented together to the llm. The metadata keys are included in the"
-                " combined documents to give the LLM more context."
-            ),
-        ),
-    ] = None,
-    iteration_limit: Annotated[
-        int,
-        Field(
-            title="Iteration limit",
-            description=(
-                "In a map-reduce summarization strategy, this is the maximum number of"
-                ' times the LLM will "summarize the summaries".'
-            ),
-        ),
-    ] = 3,
-    collapse_token_max: Annotated[
-        int,
-        Field(
-            title="Collapse token max",
-            description=(
-                "In a map-reduce summarization strategy, this is the maximum number of"
-                " tokens to include in the combined summaries that the LLM will"
-                " summarize."
-            ),
-        ),
-    ] = 6000,
+    file_filter: FileFilter,
+    preprocessor: Preprocessor,
+    summarize_map_reduce: SummarizeMapReduce,
+    api_key: str | None = Header(
+        default=None,
+        title="API key",
+        description="API key for the summarization LLM. OpenAI is default",
+    ),
 ) -> dict[str, str]:
+
     if api_key is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -178,33 +167,33 @@ async def summarize_from_disk(
 
     try:
         input_files = filter_files(
-            collection_digits=collection_digits,
+            collection_digits=file_filter.collection_digits,
             dataset=DATASET_PATH,
-            title_digits=title_digits,
+            title_digits=file_filter.title_digits,
             file_format=DatasetFileFormatNames.HTML,
         )
 
         preprocessor: Coroutine[Any, Any, list[Document]] = html_to_md_documents(
             input_files,
             PARSE_FNS["markdownify_html_to_md"],
-            max_tokens_per_doc,
-            metadata_to_include,
+            preprocessor.max_tokens_per_doc,
+            preprocessor.metadata_to_include,
         )
 
         parsed_documents = await preprocessor
 
-        if core_prompt is None:
+        if summarize_map_reduce.core_prompt is None:
             prompt = SummarizationTestPrompt.SIMPLE.value
 
         with get_openai_callback() as cb:
             summary = await map_reduce(
                 parsed_documents,
                 prompt,
-                collapse_prompt,
-                combine_prompt,
-                max_concurrency=max_concurrency,
-                iteration_limit=iteration_limit,
-                collapse_token_max=collapse_token_max,
+                summarize_map_reduce.collapse_prompt,
+                summarize_map_reduce.combine_prompt,
+                max_concurrency=summarize_map_reduce.max_concurrency,
+                iteration_limit=summarize_map_reduce.iteration_limit,
+                collapse_token_max=summarize_map_reduce.collapse_token_max,
             )
             usage_report = cb
 
@@ -221,4 +210,4 @@ async def summarize_from_disk(
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=50201, reload=True)
+    uvicorn.run("serve:app", host="127.0.0.1", port=50201, reload=True)
