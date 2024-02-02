0ce87f3211a13d5ab2b9116990d9248883cb4976
Chase <cvansteenburg@gmail.com>
2024-02-01 22:17:38 -0800

    Inject test dataset into summarize from disk, make first test pass

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,9 +4,11 @@ from unittest.mock import patch
 
 import pytest
 import time_machine
+from fastapi import HTTPException
 
 from src.models.dataset_model import DatasetFileFormatNames
 from src.services.io import (
+    FileFilter,
     filter_files,
     parse_files_from_paths,
     read_file_content,
@@ -17,9 +19,9 @@ print(f'DATASET FOR TESTING: {dataset_for_testing.__path__}')
 
 # test filter_files with tests/dataset_for_testing
 
-@pytest.fixture
-def dataset_path():
-    return Path(dataset_for_testing.__path__[0])
+# @pytest.fixture
+# def dataset_path():
+#     return Path(dataset_for_testing.__path__[0])
 
 @pytest.fixture(params=[
     (DatasetFileFormatNames.HTML, 'html-message.html'),
@@ -30,14 +32,34 @@ def dataset_path():
 def file_format_checks(request):
     return request.param
 
-def test_no_matching_collection_digits(dataset_path):
-    result = filter_files('999', dataset_path)
-    assert len(result) == 0, "Expected no files to be returned when no collection digits match."
+@pytest.fixture
+def file_filter():
+    return FileFilter(
+        collection_digits='000',
+        title_digits=['001', '002'],
+        file_format=DatasetFileFormatNames.HTML
+    )
 
-def test_no_title_digits_provided_returns_all_files(dataset_path, file_format_checks):
+def test_no_matching_collection_digits():
+    filter_inputs = FileFilter(
+        collection_digits='999'
+    )
+
+    with pytest.raises(HTTPException) as exc_info:
+        result = filter_files(filter_inputs, test_root=True)
+        assert exc_info.value.status_code == 404
+        assert "Could not find collection 999" in exc_info.value.detail
+
+def test_no_title_digits_provided_returns_all_files(file_format_checks):
     file_format, expected_file_name = file_format_checks
     expected_count = 4  # Expecting 4 HTML files, one from each test collection
-    result = filter_files('000', dataset_path, file_format=file_format)
+
+    filter_inputs = FileFilter(
+        collection_digits='000',
+        title_digits=[],
+        file_format=file_format
+    )
+    result = filter_files(filter_inputs, test_root=True)
 
     assert len(result) == expected_count, f"Expected {expected_count} files, got {len(result)}."
 
@@ -47,10 +69,15 @@ def test_no_title_digits_provided_returns_all_files(dataset_path, file_format_ch
         assert file_path.is_file(), f"Path {file_path} is not a file."
 
 # Test when both collection_digits and title_digits are provided and match subdirectories
-def test_matching_collection_and_multiple_title_digits(dataset_path, file_format_checks):
+def test_matching_collection_and_multiple_title_digits(file_format_checks):
     file_format, expected_file_name = file_format_checks
     expected_count = 2
-    result = filter_files('000', dataset_path, title_digits=['001', '999'], file_format=file_format)
+    filter_inputs = FileFilter(
+        collection_digits='000',
+        title_digits=['001', '999'],
+        file_format=file_format
+    )
+    result = filter_files(filter_inputs, test_root=True)
     
     assert len(result) == expected_count, f"Expected {expected_count} files, got {len(result)}."
     
