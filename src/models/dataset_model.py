c2ee71c4b9347fdf402b25182785607508380b0c
Chase <cvansteenburg@gmail.com>
2024-02-05 00:56:44 -0800

    remove unused data model

--- src/models/dataset_model.py
+++ src/models/dataset_model.py
@@ -1,8 +1,4 @@
 from enum import StrEnum
-from pathlib import Path
-from types import MappingProxyType
-
-import datasets
 
 
 class DatasetFileFormatNames(StrEnum):
@@ -10,22 +6,3 @@ class DatasetFileFormatNames(StrEnum):
     JSON = "json-body.json"
     RAW = "raw-source.txt"
     TXT = "text-message.txt"
-
-# Get dataset directory
-root_directory = Path(datasets.__path__[0]).resolve()
-
-def build_mapping(directory: Path) -> dict:
-    mapping: dict = {}
-    for item in directory.iterdir():
-        # Skip hidden and private
-        if item.name.startswith(('.', '__')):
-            continue
-        if item.is_dir():
-            mapping[item.name] = build_mapping(item)
-        else:
-            mapping[item.stem] = item
-    return mapping
-
-_dataset_map = build_mapping(root_directory)
-
-DATASET_MAP = MappingProxyType(_dataset_map)
